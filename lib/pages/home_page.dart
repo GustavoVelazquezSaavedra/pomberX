ea794f9a8ff14e183d3dd5d66ce888531d08ecca
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-01-16 08:49:35 -0300

    Aplicando Estilo al mapa

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,53 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_maps/utils/map_styles.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-//-25.4077991,-54.6768957,13z
-
-final CameraPosition _initialPosition =
-    CameraPosition(target: LatLng(-25.4077991, -54.6768957), zoom: 10);
 
 class _HomePageState extends State<HomePage> {
+  //-25.4077991,-54.6768957,13z
+
+  final Completer<GoogleMapController> _completer = Completer();
+
+  final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(-25.4077991, -54.6768957),
+    zoom: 15,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    this._init();
+  }
+
+  Future<GoogleMapController> get _mapController async {
+    return await _completer.future;
+  }
+
+  _init() async {
+    //
+    (await _mapController).setMapStyle(jsonEncode(mapStyle));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: GoogleMap(initialCameraPosition: _initialPosition),
+        child: GoogleMap(
+          initialCameraPosition: _initialPosition,
+          zoomControlsEnabled: false,
+          compassEnabled: false,
+          onMapCreated: (GoogleMapController controller) {
+            _completer.complete(controller);
+          },
+        ),
       ),
     );
   }
