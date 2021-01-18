349124ac0a19f969bfc45ee28dbb57c1aa6f11c1
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-01-18 08:18:52 -0300

    Permisos de ubicacion parte 2

--- lib/pages/request_permission_page.dart
+++ lib/pages/request_permission_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'home_page.dart';
 
 class RequestPermissionPage extends StatefulWidget {
   static const routeName = 'request-permission';
@@ -6,12 +9,99 @@ class RequestPermissionPage extends StatefulWidget {
   _RequestPermissionPageState createState() => _RequestPermissionPageState();
 }
 
-class _RequestPermissionPageState extends State<RequestPermissionPage> {
+class _RequestPermissionPageState extends State<RequestPermissionPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("AppLifecycleState:::::::: $state");
+    if (state == AppLifecycleState.resumed) {
+      this._check();
+    }
+  }
+
+  _check() async {
+    print("AppLifecycleState:::::::: Checking");
+    final bool hasAccess = await Permission.locationWhenInUse.isGranted;
+    if (hasAccess) {
+      this._goToHome();
+    }
+  }
+
+  _goToHome() {
+    Navigator.pushReplacementNamed(context, HomePage.routeName);
+  }
+
+  Future<void> _request() async {
+    final PermissionStatus status =
+        await Permission.locationWhenInUse.request();
+    print("Status $status");
+    switch (status) {
+      case PermissionStatus.undetermined:
+        break;
+
+      case PermissionStatus.granted:
+        this._goToHome();
+        break;
+
+      case PermissionStatus.denied:
+        break;
+
+      case PermissionStatus.restricted:
+        break;
+
+      case PermissionStatus.permanentlyDenied:
+        openAppSettings();
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("No se obtuvo el permiso"),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "PERMISO REQUERIDO",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Necesitamos permiso para acceder a su ubicación física",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            FlatButton(
+              child: Text(
+                "PERMITIR",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+              onPressed: this._request,
+            )
+          ],
+        ),
       ),
     );
   }
